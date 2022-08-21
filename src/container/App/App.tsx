f687d29b5292b1bb45c3b755106a6cfaf9cb0d0d
Andrii Osadchi <a.osadchi.07gmail.com>
2022-08-21 22:20:24 +0300

    add material ui lib

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react'
 
-type Props = {
-    title: string
-}
-
-const AppList = () => {
-    return (
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
-    )
-}
-
-const AppHeader = (props: Props) => {
-    return <h1>Hello {props.title}</h1>
-}
-
 const App = () => {
     return (
         <>
-            <AppHeader title="App.js" />
-            <AppHeader title="React.js" />
-            <AppList />
+            <h1>Hello App</h1>
         </>
     )
 }
